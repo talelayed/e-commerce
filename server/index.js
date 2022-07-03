b44d158ba91f7913a39cca53b70aa9997c05145f
talel <you@example.com>
2022-07-03 17:22:49 +0100

    back

--- server/index.js
+++ server/index.js
@@ -4,6 +4,25 @@ const app = express();
 
 const mongoose = require('mongoose');
 
-app.listen(5000, () => {
+const dotenv = require('dotenv');
+
+const userRoute = require('./routes/user');
+const authRoute = require('./routes/auth');
+
+dotenv.config();
+
+mongoose
+    .connect(
+        process.env.MONGO_URL
+    )
+    .then(()=> console.log("MongoDb connected."))
+    .catch((err)=>{console.log(err);});
+
+app.use(express.json());
+
+app.use("/api/user", userRoute);
+app.use("/api/auth", authRoute);
+
+app.listen(process.env.PORT || 5000, () => {
     console.log("Server listening...");
 })
